172cd33821c198c06f98611a14bfc6c90a561d39
Dominic Thompson <DWThompson2@student.fullsail.edu>
2025-07-16 15:28:35 -0400

    hello world prints

--- Frame.cpp
+++ Frame.cpp
@@ -2,5 +2,5 @@
 
 Frame::Frame(const wxString& title) : wxFrame(nullptr, wxID_ANY, "To-Do")
 {
-	
+	wxStaticText* textArea = new wxStaticText(this, wxID_ANY, "Hello World", wxPoint(10, 10), wxSize(480, 180), wxCENTER);
 }
